09f43faab79435dc8c57674a1d4292ab5a8b0bf3
skysmile3 <isaaclee1101@hanmail.net>
2018-11-27 15:03:33 +0900

    Nodejs Practice

---
+++ syntax/comparison.js
@@ -0,0 +1,9 @@
+console.log(1==1);
+console.log(1==2);
+console.log(1>2);
+console.log(1<2);
+console.log(1===1);
+console.log(1===2);
+name = 1; //대입
+name == 1; //비교
+name === 1; //두 개를 쓸 때는 꼭 세 개를 써라?
